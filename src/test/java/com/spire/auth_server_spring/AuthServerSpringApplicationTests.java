d2211e871901a68006cfe08b3244204e905e88d5
Garret <caerus1001@gmail.com>
2024-01-29 20:48:53 -0700

    Initial commit to get Spring OAuth2 server working

---
+++ src/test/java/com/spire/auth_server_spring/AuthServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spire.auth_server_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
